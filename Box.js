8f051c07aa284800231ecc48c36e2e7f072c5373
vriks2008 <81859713+vriks2008@users.noreply.github.com>
2021-05-19 20:11:09 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,30 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        var option={
+            'restitution' : 0.8,
+            'friction': 1.0,
+            'density': 0.1
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        World.add(world,this.body);
+        
+        this.width=width;
+        this.height=height;
+    }
+
+    display()
+    {
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("brown")
+     rect(0,0,this.width,this.height);
+        pop();     
+    }
+}
+
